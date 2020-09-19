2ca47d03eba91422687b40369d9455f33beb643f
yun-hyun-joon <hihelper@naver.com>
2020-09-20 02:32:04 +0900

    커밋

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, redirect
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        return render(request, 'register.html')
