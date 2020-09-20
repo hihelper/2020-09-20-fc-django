b1d9090aadeb79f7454aee5ad21887331300b9c5
yun-hyun-joon <hihelper@naver.com>
2020-09-20 14:32:49 +0900

    커밋

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -7,13 +7,12 @@ from django.db import models
 class Fcuser(models.Model):
     username = models.CharField(max_length=64,
                                 verbose_name='사용자명')
+    useremail = models.EmailField(max_length=128,
+                                  verbose_name='사용자이메일')
     password = models.CharField(max_length=64,
                                 verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True,
                                            verbose_name='등록시간')
-    useremail = models.EmailField(max_length=128,
-                                  verbose_name='사용자이메일',
-                                  blank=True)
 
     # 이 부분은 어드민 페이지 내부에서 각 데이터베이스 하나하나가 보여지는 문구를 정해주는 것이다. 이걸 설정 안하면 그냥 단순히 object01..02..03.. 식으로만 보일 뿐이다.
     def __str__(self):
